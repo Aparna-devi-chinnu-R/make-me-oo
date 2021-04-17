ab98778a60476247005813f599e11c279e264674
Aparna devi chinnu R <aparnadevi.rajavel@thoughtworks.com>
2021-04-16 23:17:28 -0700

    [Aparna] Creating a method named distance in point class

--- src/main/java/org/oop/Point.java
+++ src/main/java/org/oop/Point.java
@@ -4,24 +4,23 @@ public class Point {
     private double x;
     private double y;
 
-    public double getX() {
+    public double getX()
+    {
         return x;
     }
 
-    public void setX(double x) {
-        this.x = x;
-    }
-
-    public double getY() {
-        return y;
-    }
-
-    public void setY(double y) {
+    public void setY(double y)
+    {
         this.y = y;
     }
 
-    public Point(double x, double y) {
+    public Point(double x, double y)
+    {
         this.x = x;
         this.y = y;
+    }
+    public static double distance(Point p)
+    {
+          return 0.0;
     }
 }
